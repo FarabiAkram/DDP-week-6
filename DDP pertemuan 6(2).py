9d9dc999695a1815a20094504a1f872b419d0278
FarabiAkram <145969289+FarabiAkram@users.noreply.github.com>
2023-11-06 21:43:39 +0700

    DDP week 6

---
+++ DDP pertemuan 6(2).py
@@ -0,0 +1,10 @@
+# Inisialisasi variabel
+n = 10  # Jumlah angka dalam deret
+a = 1   # Nilai awal deret
+d = 2   # Selisih antara angka-angka dalam deret
+
+# Hitung jumlah deret
+sum_of_series = n / 2 * (2 * a + (n - 1) * d)
+
+# Cetak hasil
+print(f"1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19 = {sum_of_series}")
